c49413152e1bc8dca13c1259857e72cd13499e24
Demsiegetenet <demiseget@gmail.com>
2025-10-12 11:43:30 -0500

    "first user login commit"

---
+++ src/test/java/com/moviereview/repository/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.moviereview.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.moviereview.userlogin.model.User;
+import com.moviereview.userlogin.repository.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class UserRepositoryTest {
+
+    private final UserRepository userRepository;
+
+    public UserRepositoryTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Test
+    public void saveUser() {
+        User user = new User();
+        user.setEmail("john.doe@example.com");
+        user.setPassword("password");
+
+        User savedUser = userRepository.save(user);
+        assertNotNull(savedUser);
+        assertNotNull(savedUser.getId());
+    }
+
+}
