69d93c2cfc4966d8045c43ed5af7c72e4a0027a1
Demsiegetenet <demiseget@gmail.com>
2025-10-21 16:41:23 -0500

    code clean

--- src/main/java/com/moviereview/exception/ErrorResponse.java
+++ src/main/java/com/moviereview/exception/ErrorResponse.java
@@ -2,6 +2,11 @@ package com.moviereview.exception;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timestamp;
     private int status;
@@ -18,47 +23,6 @@ public class ErrorResponse {
         this.status = status;
         this.error = error;
         this.message = message;
-        this.path = path;
-    }
-
-    // Getters and Setters
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
         this.path = path;
     }
 }
